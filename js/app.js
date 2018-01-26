bb347057f88e5bf45239497a47886969fe7514f7
max-jellycat <max@jellycat.fr>
2018-01-26 17:29:33 +0100

    Dynamic btn generation + player hand selection

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var app = {
   init: function() {
     $('.btn-collapse').html('<i class="fas fa-arrow-down"></i>');
+    app.createButtons();
     $('.btn-collapse').on('click', app.collapse);
+    $('.hands .btn').on('click', app.showPlayerHand);
   },
 
   collapse: function() {
@@ -15,9 +17,27 @@ var app = {
       $(this).html('<i class="fas fa-arrow-up"></i>');
     }
     $toCollapse.slideToggle();
+  },
+
+  createButtons: function() {
+    for(var hand in app.hands) {
+      var btn = '<button class="btn" data-hand="'+ app.hands[hand] +'"><i class="fas fa-hand-'+ app.hands[hand] +'"></i> '+ app.hands[hand] +'</button>'
+      console.log(btn);
+      $('.hands').append(btn);
+      $('.hands .btn').css('text-transform', 'capitalize');
+    }
+  },
+
+  showPlayerHand: function() {
+    for(var hand in app.hands) {
+      var handIcon = '<i class="fas fa-hand-'+ app.hands[hand] +'"></i>'
+      var $container = $('.player .hand-container');
+      if(app.hands[hand] === $(this).data('hand'))
+        $container.html(handIcon);
+    }
   }
 }
 
-app.hands = ['rock', 'paper', 'scissors', 'lizard', 'spoke'];
+app.hands = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 $(app.init);
